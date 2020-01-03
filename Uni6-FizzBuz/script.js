844a9f14181a77aee11f3c6dbb7c5332c26fd2dd
Mary Dheey <maria.limadh@hotmail.com>
2020-01-02 21:38:56 -0300

    Atualização Uni6 e inicio Uni7

--- Uni6-FizzBuz/script.js
+++ Uni6-FizzBuz/script.js
@@ -6,24 +6,24 @@ function executaFizzBuzz(){
     var numIteracoes = document.getElementById("numIteracoes").value
     var resultado = document.getElementById("resultado")
 
-    alert("O numero de iterações é: " + numIteracoes)
+    //alert("O numero de iterações é: " + numIteracoes)
 
     var numero = 1
     while(numero <= numIteracoes){
         if(((numero % 5) == 0) && (numero % 3) == 0){
             //console.log("FizzBuzz")
-            //USANDO A DIV
-            resultado = document.write(numero + "- FizzBuzz" + "</br>")
+            //USANDO O DIV
+            resultado = document.writeln(numero + "- FizzBuzz" + "</br>")
 
         }else if((numero % 3) == 0){
             //console.log("Fizz")
-            resultado = document.write(numero + "- Fizz" + "</br>")
+            resultado = document.writeln(numero + "- Fizz" + "</br>")
         }else if((numero % 5) == 0){
             //console.log("Buzz")
-            resultado = document.write(numero + "- Buzz" + "</br>")
+            resultado = document.writeln(numero + "- Buzz" + "</br>")
         }else {
             //console.log(numero)
-            resultado = document.write(numero + "</br>")
+            resultado = document.writeln(numero + "</br>")
         }
         numero++
     }
---
+++ Uni7-Album/script.js
@@ -0,0 +1,16 @@
+var colecao = [1, 5, "A", new Image(), 2]
+//mostra a quantidade de elementos na colecao
+console.log(colecao.length)
+//'empurra', insere novos elementos para a colecao
+colecao.push(10)
+console.log(colecao.length)
+//mostra acolecao inteira
+console.log(colecao)
+//remove elemento da colecao
+colecao.pop()
+console.log(colecao)
+//assessa determinado elemento detro da colecao
+console.log(colecao[0])
+console.log(colecao[2])
+//assessando o ultimo elemento
+console.log(colecao[colecao.length-1])
